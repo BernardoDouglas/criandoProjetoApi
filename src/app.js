94758caa7a38cc9c3ca4a09f247fd584bc256a34
Douglas Bernardo <douglasbernardo01@hotmail.com>
2021-06-29 16:28:35 -0300

    criando projeto api

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express') 
+const app = express() 
+const cors = require('cors')
+
+const alunos = [
+  {
+    nome: 'Aluno1',
+    idade: 12
+  },
+  {
+    nome: 'Aluno2',
+    idade: 13
+  },
+  {
+    nome: 'Aluno3',
+    idade: 14
+  },
+  {
+    nome: 'Aluno4',
+    idade: 15
+  }
+]
+app.use(cors()) 
+
+app.get('/api/v1/alunos', function (req, res) { 
+  res.send(alunos)
+})
+
+app.listen(3000) 
